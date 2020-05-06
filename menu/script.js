832c6cfcaae7ea2cee42bb6719bf2f7f2e51f433
esraashabrani <esraashabrani@gmail.com>
2020-05-06 21:30:22 +0300

    version3

---
+++ menu/script.js
@@ -0,0 +1,72 @@
+var starters = [{
+	name : "GARLIC BREAD SUPREME",
+	price : 1.50
+},
+{
+	name:"TRIO PLATTER",
+	price:4.00
+},
+{
+	name:"CHICKEN BBQ ROLLS",
+	price:2.25
+},
+{
+	name:"WEDGES",
+	price:1.75
+},
+{
+	name:"MOZZARELLA STICKS",
+	price:3.00
+}
+];
+
+var salad=[{
+    name : "SALAD BAR LARGE",
+	price : 3.75
+},
+{
+    name : "SALAD BAR SMALL",
+	price : 3.00
+},
+];
+
+var drinks=[{
+    name : "MIRINDA",
+	price : 0.90
+},
+{
+    name : "7UP",
+	price : 0.90
+},
+{
+    name : "PEPSI",
+	price : 0.90
+},
+{
+    name : "DIET PEPSI",
+	price : 0.90
+},
+];
+
+var pizza=[{
+    name : "PEPPERONI",
+	price : 5.75
+},
+{
+    name : "SUPER SUPREME",
+	price : 6.00
+},
+{
+    name : "CHICKEN FAJITA",
+	price : 6.00
+},
+{
+    name : "GRILLED CHICKEN",
+	price : 6.00
+},
+{
+    name : "MARGHERITA",
+	price : 4.75
+}
+];
+
